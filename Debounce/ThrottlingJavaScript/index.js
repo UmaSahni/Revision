2ac2c06a3b5771c0a5055013f25c274e161eca8a
Uma Sahni <monkeytown64@gmail.com>
2023-05-15 00:01:26 +0530

    Throttling Code

---
+++ Debounce/ThrottlingJavaScript/index.js
@@ -0,0 +1,52 @@
+const expensiveFunction = () => {
+  // This function is expensive
+  console.log("This is expensive function");
+};
+
+// ! window.addEventListener("resize", expensiveFunction);
+
+// suppose this expensive function is called each every time when the window is resized. When the window is resized then thousands of milliseconds events are triggered and thousands of api calls happed. What if this expensive function could be better.
+
+// Now instead of calling expensiveFunction we call betterExpensiveFunction
+
+const throttleFunction = (fun, delay) =>{
+    let flag = true;
+
+    return function (){
+        if(flag){
+        fun()
+         falg = false
+        }
+
+        setTimeout(()=>{
+            falg = true
+        }, delay);
+    }
+}
+
+const  betterExpensiveFunction = throttleFunction(expensiveFunction, 1000);
+
+window.addEventListener("resize", betterExpensiveFunction);
+
+// const expensiveFunction = () => {
+//   // This function is expensive
+//   console.log("This is expensive function");
+// };
+
+// const throttleFunction = (fun, delay) => {
+//   let flag = true;
+
+//   return function () {
+//     if (flag) {
+//       fun();
+//       flag = false;
+//     }
+//     setTimeout(() => {
+//       flag = true;
+//     }, delay);
+//   };
+// };
+
+// const betterExpensiveFunction = throttleFunction(expensiveFunction, 1000);
+
+// window.addEventListener("resize", betterExpensiveFunction);
