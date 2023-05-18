df8678be8eb5ab1329593db403e93d3b87009977
Uma Sahni <monkeytown64@gmail.com>
2023-05-18 11:09:18 +0530

    Local Storage

--- Local-Storage/Todo/index.js
+++ Local-Storage/Todo/index.js
@@ -1,8 +1,9 @@
 document.querySelector("form").addEventListener("submit", myTodo)
-let todoArr = JSON.parse(localStorage.getItem("todo")) || []
+let todoArr =  JSON.parse(localStorage.getItem("todo")) || []
 displayTable(todoArr)
-function myTodo (event){
-    event.preventDefault()
+let favArr = JSON.parse(localStorage.getItem("fav")) || []
+function myTodo(event){
+    event.preventDefault();
     let taskName = document.querySelector("#task").value;
     let taskpriority = document.querySelector("#priority").value;
     let taskObj = {
@@ -13,17 +14,30 @@ function myTodo (event){
     displayTable(todoArr)
 }
 
-function displayTable (todoArr){
+function displayTable(todoArr) {
     document.querySelector("tbody").innerHTML = ""
     todoArr.forEach((el)=>{
-        let row  =  document.createElement("tr")
+        console.log(el)
+        let row = document.createElement("tr")
         let td1 = document.createElement("td")
         td1.innerText = el.taskName
         let td2 = document.createElement("td")
         td2.innerText = el.taskpriority
+
+        if(el.taskpriority==="High"){
+            td2.style.backgroundColor = "red"
+        }
+        else{
+            td2.style.backgroundColor = "green"
+        }
         let td3 = document.createElement("td")
-        td3.innerText = "Add as fav"
+        td3.innerText = "add as fav"
+        td3.addEventListener("click",()=>{
+            favArr.push(el)
+            localStorage.setItem('fav', JSON.stringify(favArr) )
+            console.log(favArr)
+        } )
         row.append(td1, td2, td3)
-        document.querySelector("tbody").append(row)
+         document.querySelector("tbody").append(row)
     })
 }
--- Local-Storage/readme.md
+++ Local-Storage/readme.md
@@ -1,10 +1,19 @@
  # Local Storage
- It will only allow to store strings and numbers.
+
+It will only allow to store strings and numbers.
  
  ```js
 localStorage.setItem("name", "uma")
 localStorage.getItem("name")
  ```
 
- To reslove this problem there is JSON->javaScript object notation
- 
+To reslove this problem there is JSON->javaScript object notation
+
+ ```js
+ let arr = [1,2,3,4]
+
+localStorage.setItem("arr", JSON.stringify(arr))
+let out JSON.parse(localStrorage.getItem("arr"))
+console.log(out)
+
+ ```
