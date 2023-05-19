b8e45f0ca690c8be0eaa40ac6ce12b4670eee5a8
Uma Sahni <monkeytown64@gmail.com>
2023-05-20 04:21:40 +0530

    Search funtionality in fakestore api

--- Fetch/fakestore/index.js
+++ Fetch/fakestore/index.js
@@ -1,4 +1,43 @@
+let bag = []
 fetch("https://fakestoreapi.com/products")
-.then((res)=>res.json())
-.then((data)=>console.log(data))
-.catch((err)=>console.log(err))
+  .then((res) => res.json())
+  .then((data) => {displayTable(data)
+bag=data
+})
+  .catch((err) => console.log(err));
+
+const displayTable = (data) => {
+  console.log(data)
+document.getElementById("container").innerHTML = ""
+  data.forEach((el) => {
+    // <img src="" />
+    // <h2>Title</h2>
+    // <p>Price</p>
+    // <p>Rating</p>
+    let div = document.createElement("div")
+    
+    let image = document.createElement("img");
+    image.setAttribute("src", el.image)
+
+    let title = document.createElement("h2");
+    title.innerText = el.title
+    let price = document.createElement("p");
+    price.innerText = el.price
+
+    let rating = document.createElement("p");
+    rating.innerText = el.rating.rate
+
+    div.append(image, title, price, rating)
+    document.getElementById("container").append(div)
+  });
+};
+
+// ! Search funtionality
+document.querySelector("input").addEventListener("input", ()=>{
+ let q =   document.querySelector("input").value
+ let newData = bag.filter((el)=>{
+    return el.title.toLowerCase().includes(q.toLowerCase())
+ })
+ displayTable(newData)
+ console.log(newData)
+})
